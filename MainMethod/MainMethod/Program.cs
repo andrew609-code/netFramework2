34c28c896534d9748de3294b791e851c727a20b2
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-12-15 10:25:01 -0800

    MethodAssign

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathMethod2 class
+            MathMethod2 math1 = new MathMethod2();
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(7));
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(7.3m));
+            //Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("7"));
+        }
+        
+    }
+}
